1d83ba58a64b112f49234394d0a5da0e5a678364
Valentin Richard <valentin.richard@infomaniak.com>
2023-12-18 14:43:50 +0100

    wip

---
+++ src/todo-list/components/todo-list/observables/todo-list-with-observables.component.ts
@@ -0,0 +1,93 @@
+import { $$map, IObservable, IObserver, let$$, map$$ } from '@lirx/core';
+import { compileReactiveHTMLAsComponentTemplate, compileStyleAsComponentStyle, Component } from '@lirx/dom';
+import { TodoListItemComponent } from '../../todo-list-item/todo-list-item.component';
+import { ITodoListItem, ITodoListItemsList } from '../todo-list.types';
+
+
+// @ts-ignore
+import html from './todo-list-with-observables.component.html?raw';
+// @ts-ignore
+import style from './todo-list.component.scss?inline';
+
+/**
+ * COMPONENT: 'app-todo-list-with-observables'
+ **/
+
+interface ITemplateData {
+  readonly inputValue$: IObservable<string>;
+  readonly $onInput: IObserver<Event>;
+  readonly onFormSubmit$: IObservable<IObserver<Event>>;
+
+  readonly items$: IObservable<ITodoListItemsList>;
+
+  readonly removeItem: (item: ITodoListItem) => void;
+}
+
+export const TodoListWithObservablesComponent = new Component({
+  name: 'app-todo-list-with-observables',
+  template: compileReactiveHTMLAsComponentTemplate({
+    html,
+    components: [
+      TodoListItemComponent,
+      // TodoListItemShadowComponent,
+    ],
+  }),
+  styles: [compileStyleAsComponentStyle(style)],
+  templateData: (): ITemplateData => {
+    const [$inputValue, inputValue$] = let$$<string>('');
+    const [$items, items$, getItems] = let$$<ITodoListItemsList>([]);
+
+    const addItem = (
+      message: string,
+    ): void => {
+      $items([
+        ...getItems(),
+        {
+          message,
+        },
+      ]);
+    };
+
+    const removeItem = (
+      item: ITodoListItem,
+    ): void => {
+      const items: ITodoListItemsList = getItems();
+      const index: number = items.indexOf(item);
+      if (index !== -1) {
+        $items([
+          ...items.slice(0, index),
+          ...items.slice(index + 1),
+        ]);
+      }
+    };
+
+    const $onInput = $$map($inputValue, (event: Event): string => (event.target as HTMLInputElement).value);
+
+    const onFormSubmit$ = map$$(inputValue$, (inputValue: string): IObserver<Event> => {
+      inputValue = inputValue.trim();
+
+      return (
+        event: Event,
+      ): void => {
+        event.preventDefault();
+
+        if (inputValue !== '') {
+          addItem(inputValue);
+        }
+
+        $inputValue('');
+      };
+    });
+
+    addItem('Check this awesome tutorial');
+    addItem('Write your own components');
+
+    return {
+      inputValue$,
+      $onInput,
+      onFormSubmit$,
+      items$,
+      removeItem,
+    };
+  },
+});
--- src/todo-list/components/todo-list/todo-list.component.ts
+++ src/todo-list/components/todo-list/todo-list.component.ts
@@ -1,91 +1,91 @@
-import { $$map, IObservable, IObserver, let$$, map$$ } from '@lirx/core';
+import { signal, ISignal, $log, unknownToObservableStrict } from '@lirx/core';
 import { compileReactiveHTMLAsComponentTemplate, compileStyleAsComponentStyle, Component } from '@lirx/dom';
+import { Writable } from '@lirx/utils';
 import { TodoListItemComponent } from '../todo-list-item/todo-list-item.component';
-import { ITodoListItemsList, ITodoListItem } from './todo-list.types';
+import { ITodoListItem, ITodoListItemsList } from './todo-list.types';
 
 // @ts-ignore
 import html from './todo-list.component.html?raw';
 // @ts-ignore
-import style from './todo-list.component.scss?inline';
+import style from '../todo-list.component.scss?inline';
 
 /**
- * COMPONENT: 'app-todo'
+ * COMPONENT: 'app-todo-list'
  **/
 
 interface ITemplateData {
-  readonly inputValue$: IObservable<string>;
-  readonly $onInput: IObserver<Event>;
-  readonly onFormSubmit$: IObservable<IObserver<Event>>;
+  readonly inputValue: ISignal<string>;
+  readonly onInput: (event: Event) => void;
+  readonly onFormSubmit: (event: Event) => void;
 
-  readonly items$: IObservable<ITodoListItemsList>;
+  readonly items: ISignal<ITodoListItemsList>;
 
   readonly removeItem: (item: ITodoListItem) => void;
 }
 
-export const TodoListComponent = new Component({
+export const TodoList = new Component({
   name: 'app-todo-list',
   template: compileReactiveHTMLAsComponentTemplate({
     html,
     components: [
       TodoListItemComponent,
-      // TodoListItemShadowComponent,
     ],
   }),
   styles: [compileStyleAsComponentStyle(style)],
   templateData: (): ITemplateData => {
-    const [$inputValue, inputValue$] = let$$<string>('');
-    const [$items, items$, getItems] = let$$<ITodoListItemsList>([]);
+    /* ITEMS */
+
+    const items = signal<ITodoListItemsList>([]);
 
     const addItem = (
       message: string,
     ): void => {
-      $items([
-        ...getItems(),
-        {
+      items.mutate((items: Writable<ITodoListItemsList>): void => {
+        items.push({
           message,
-        },
-      ]);
+        });
+      });
     };
 
     const removeItem = (
       item: ITodoListItem,
     ): void => {
-      const items: ITodoListItemsList = getItems();
-      const index: number = items.indexOf(item);
+      const index: number = items().indexOf(item);
       if (index !== -1) {
-        $items([
-          ...items.slice(0, index),
-          ...items.slice(index + 1),
-        ]);
+        items.mutate((items: Writable<ITodoListItemsList>): void => {
+          items.splice(index, 1);
+        });
       }
     };
-
-    const $onInput = $$map($inputValue, (event: Event): string => (event.target as HTMLInputElement).value);
-
-    const onFormSubmit$ = map$$(inputValue$, (inputValue: string): IObserver<Event> => {
-      inputValue = inputValue.trim();
-
-      return (
-        event: Event,
-      ): void => {
-        event.preventDefault();
-
-        if (inputValue !== '') {
-          addItem(inputValue);
-        }
-
-        $inputValue('');
-      };
-    });
 
     addItem('Check this awesome tutorial');
     addItem('Write your own components');
 
+    /* INPUT */
+
+    const inputValue = signal<string>('');
+
+    const onInput = (event: Event): void => {
+      inputValue.set((event.target as HTMLInputElement).value);
+    };
+
+    const onFormSubmit = (event: Event): void => {
+      event.preventDefault();
+
+      const value = inputValue().trim();
+
+      if (value !== '') {
+        addItem(value);
+      }
+
+      inputValue.set('');
+    };
+
     return {
-      inputValue$,
-      $onInput,
-      onFormSubmit$,
-      items$,
+      inputValue,
+      onInput,
+      onFormSubmit,
+      items,
       removeItem,
     };
   },
